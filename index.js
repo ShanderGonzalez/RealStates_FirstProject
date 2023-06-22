60bc2a72b5c2af8c828dce15ce74e5e0171b0b7f
Shander <kevin.gonzalez@epn.edu.ec>
2023-06-21 21:10:20 -0500

    Create of template engine

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ import usuarioRoutes from "./routes/usuarioRoutes.js";
 //Crear la app
 const app = express()
 
+//Habilitar Pug
+app.set('view engine', 'pug') //quiero utilizar pug
+app.set('views', './views') //y en esta carpeta vas a encontrar los archivos para que los renderices y los trates como un template engine de pug
+
 //Routing
-app.use('/', usuarioRoutes)
+app.use('/auth', usuarioRoutes)
 
 //Definir puerto y arranque del proycto
 const port = 3000;
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -3,12 +3,8 @@ import express from "express";
 const router = express.Router()
 
 //Routing   -> / ruta, function es un callback
-router.get('/', (req, res) => {
-    res.json({msg: 'Hola mundo en express'})
-});
-
-router.post('/', (req, res) => {
-    res.send({msg: 'Respuesta de tipo Post'})
+router.get('/login', (req, res) => {
+    res.render('auth/login')
 });
 
 export default router
